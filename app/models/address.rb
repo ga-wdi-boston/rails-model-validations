7a3bdf8ed25136b661b42fb9b40e221047d5526e
MattB-GA-Boston <matt.brendzel@generalassemb.ly>
2015-11-04 02:03:45 -0500

    Clears out models, migrations, controllers from previous example.
    I want to start the demo with a clean slate.

--- app/models/address.rb
+++
@@ -1,4 +0,0 @@
-class Address < ActiveRecord::Base
-  belongs_to :person, inverse_of: :addresses
-  belongs_to :place, inverse_of: :addresses
-end
--- app/models/place.rb
+++
@@ -1,4 +0,0 @@
-class Place < ActiveRecord::Base
-  has_many :people, through: :addresses
-  has_many :addresses
-end
